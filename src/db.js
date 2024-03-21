7cc94f787866007b0acc769162caa64dece65579
Jesusrm2 <135676762+Jesusrm2@users.noreply.github.com>
2024-03-21 15:39:24 -0500

    Update db.js

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () =>{
     try {
-        await mongoose.connect('mongodb+srv://jesusmald23:9ZxMuBc3dOrRe4eu@cluster0.vdtw82i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+        await mongoose.connect('Your_bd_mongodb')
         console.log(">>> DB is connected")
     } catch (error) {
         console.log(error)
